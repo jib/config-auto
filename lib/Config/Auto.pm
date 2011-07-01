fd20d304fdecd677b9cc761bc6cf10fecddede53
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-07-01 14:40:19 +0100

    Bump version to 0.36 and update Changes

--- lib/Config/Auto.pm
+++ lib/Config/Auto.pm
@@ -7,7 +7,7 @@ use Carp qw[croak];
 
 use vars qw[$VERSION $DisablePerl $Untaint $Debug];
 
-$VERSION        = '0.34';
+$VERSION        = '0.36';
 $DisablePerl    = 0;
 $Untaint        = 0;
 $Debug          = 0;
